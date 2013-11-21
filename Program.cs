5116323e2ba0bad145771b3c76d39b9fa30350db
logrcubed <trilokdude@gmail.com>
2013-11-21 20:10:22 +0530

    Hosts a simple webservice

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.ServiceModel;
+using System.ServiceModel.Description;
+
+namespace RestImageProcessor
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string baseAddress = "http://localhost:8000/Service";
+            ServiceHost host = new ServiceHost(typeof(ImageUploadService), new Uri(baseAddress));
+            host.AddServiceEndpoint(typeof(IImageUpload), new WebHttpBinding(), "").Behaviors.Add(new WebHttpBehavior());
+            //host.AddServiceEndpoint(typeof(IImageUpload), new WebHttpBinding(), "").Behaviors.Add(new WebHttpBehavior());
+            host.Open();
+            Console.WriteLine("Host opened");
+            Console.ReadKey(true);
+        }
+    }
+}
